32c0b4b2ba955863d12ca259d173a1aa59705818
Josh Maldonado <joshm@topdrawermerch.com>
2024-01-23 09:21:49 -0700

    update commit

--- api/getTokensBurn.js
+++ api/getTokensBurn.js
@@ -1,11 +1,11 @@
 
   async function fetchTokensBurn(address) {
     const result = fetch('/gettokendata/'+address).then(function(response) {
-      console.log("RES STATUS: "+response.status )
+
       return response.json();
       
     }).then(function(jsonData) {
-      console.log("RES DATA: "+ JSON.stringify(jsonData) )
+
       return {
           jsonData
       }
