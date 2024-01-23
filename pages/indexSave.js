f843f97a85104446092229d75474476aac459045
Josh Maldonado <joshm@topdrawermerch.com>
2024-01-23 15:56:33 -0700

    update commit

--- pages/indexSave.js
+++ pages/indexSave.js
@@ -8,8 +8,29 @@ function Page() {
   
 
   
- 
-
+ if(isConnected){
+  
+  
+  return (
+    <div className='body-section'>
+      <div className='page-header'>
+        <div className='header-content'>
+          <div id="home-button" className='logo-content'>
+            <img src="handshake.svg" className='logo-img'/>
+          </div>
+          <div className='wallet-button connected'><ConnectKitButton /></div>
+        </div>
+      </div>
+      <div className='main-content'>
+        {isConnected && <Account address={ account } connected={ isConnected } ></Account>}
+      </div>
+      <div className='footer'>
+        <p className='footer-text'>Web3 & verification by Top Drawer Merch</p>
+      </div>
+    </div>
+  )
+ }
+else {
   return (
     <div className='body-section'>
       <div className='page-header'>
@@ -25,7 +46,9 @@ function Page() {
             <h1 className="z-10 text-5xl md:text-9xl leading-solid">INVISIBLE<br/>FRIENDS<span className="absolute text-base align-top md:text-xl">NFT</span></h1>
             <h2 className="uppercase md:text-5xl">Physical Collectible Burn</h2>
           </div>
-          <h2 className='heading-text h2'>Coming Soon</h2>
+          <h2 className='heading-text h2'>Connect Wallet</h2>
+          <p className='paragraph welcome'>Only owners of a Invisible Friends Physical Collectible Token are allowed to access the Physical Collectible Burn. Please connect your wallet now to verify NFT owenership.</p>
+          <div className='not-nav'><ConnectKitButton /></div>
         </div>
       </div>
       <div className='footer'>
@@ -33,7 +56,7 @@ function Page() {
       </div>
     </div>
   )
-
+}
 }
 
 export default Page
